dc2ae12e8601f3b1f939beae9802a006703d672c
Serhii <s.saviuk@apiko.com>
2024-07-26 14:38:37 +0300

    hide env

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { Button } from '../ui'
 import { Container } from './container'
 import { ArrowRight, ShoppingCart, User } from 'lucide-react'
+import Link from 'next/link'
 
 interface Props {
     className?: string
@@ -14,17 +15,24 @@ export const Header: React.FC<Props> = ({ className }) => {
         <header className={cn('border border-b', className)}>
             <Container className="flex items-center justify-between py-8">
                 {/* Left part */}
-                <div className="flex items-center gap-4">
-                    <Image src="/logo.png" width={35} height={35} alt="logo" />
-                    <div>
-                        <h1 className="text-2xl uppercase font-black">
-                            Next Pizza
-                        </h1>
-                        <p className="text-sm text-gray-400 leading-3">
-                            Taste awesome
-                        </p>
+                <Link href="/">
+                    <div className="flex items-center gap-4">
+                        <Image
+                            src="/logo.png"
+                            width={35}
+                            height={35}
+                            alt="logo"
+                        />
+                        <div>
+                            <h1 className="text-2xl uppercase font-black">
+                                Next Pizza
+                            </h1>
+                            <p className="text-sm text-gray-400 leading-3">
+                                Taste awesome
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 {/* Right part */}
                 <div className="flex items-center gap-3">
                     <Button
