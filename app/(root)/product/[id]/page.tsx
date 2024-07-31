7fe141dda52ad9aa9ff307c7ef9c4af5a139c3f4
Serhii <s.saviuk@apiko.com>
2024-07-31 12:34:00 +0300

    fix build error

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -54,7 +54,6 @@ export default async function ProductPage({
                                 disabled: true,
                             },
                         ]}
-                        selectedValue="2"
                     />
                 </div>
             </div>
