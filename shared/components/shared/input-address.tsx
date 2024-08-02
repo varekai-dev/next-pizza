5342c7f046e5451bd616aa251e504adf1e841d2e
Serhii <s.saviuk@apiko.com>
2024-08-03 00:33:06 +0300

    fix for google autocomplete

--- shared/components/shared/input-address.tsx
+++ shared/components/shared/input-address.tsx
@@ -1,43 +1,38 @@
 'use client'
 
 import React from 'react'
-import { useLoadScript, StandaloneSearchBox } from '@react-google-maps/api'
+import { useLoadScript, Autocomplete } from '@react-google-maps/api'
 import { FormInput } from './form'
 import { Skeleton } from '../ui'
-
-const libraries = ['places']
-
-const ternopilBounds = {
-    north: 49.609,
-    south: 49.52,
-    east: 25.644,
-    west: 25.507,
-}
+import { useFormContext } from 'react-hook-form'
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
+type AutocompleteType = google.maps.places.Autocomplete
+
 const AddressAutocomplete = () => {
-    const [value, setValue] = React.useState<string | undefined>('')
+    const { trigger } = useFormContext()
     const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: String(API_KEY),
-        libraries: libraries as any,
+        libraries: ['places'],
         language: 'en',
     })
 
-    const searchBoxRef = React.useRef<google.maps.places.SearchBox | null>(null)
+    const autocompleteRef = React.useRef<any>()
 
-    const handlePlaceChanged = () => {
-        if (searchBoxRef.current && searchBoxRef.current.getPlaces) {
-            const places = searchBoxRef.current.getPlaces()
-            console.log('places', places)
-            if (!places || places?.length === 0) {
-                return
-            }
-            const place = places[0]
-            console.log('place', place)
-            setValue(place.formatted_address)
-        }
-    }
+    const onPlaceChanged = React.useCallback(() => {
+        const place = autocompleteRef?.current?.getPlace()
+
+        trigger('address')
+    }, [trigger])
+
+    const onLoad = React.useCallback(
+        (autocomplete: AutocompleteType) => {
+            autocompleteRef.current = autocomplete
+            autocomplete.addListener('place_changed', onPlaceChanged)
+        },
+        [onPlaceChanged]
+    )
 
     if (loadError) {
         return <div>Error loading Google Maps</div>
@@ -47,26 +42,28 @@ const AddressAutocomplete = () => {
         return <Skeleton className="h-12" />
     }
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value)
-    }
-
     return (
         <div className="w-full relative">
-            <StandaloneSearchBox
-                ref={searchBoxRef as any}
-                bounds={ternopilBounds}
-                onPlacesChanged={handlePlaceChanged}
+            <Autocomplete
+                onPlaceChanged={onPlaceChanged}
+                onLoad={onLoad}
+                fields={['address_components', 'formatted_address']}
+                options={{
+                    types: ['address'], // Restrict to addresses
+                    componentRestrictions: { country: 'ua' }, // Restrict to Ukraine
+                    bounds: new window.google.maps.LatLngBounds(
+                        new window.google.maps.LatLng(49.528, 25.553), // Southwest corner of Ternopil
+                        new window.google.maps.LatLng(49.568, 25.623) // Northeast corner of Ternopil
+                    ),
+                }}
             >
                 <FormInput
-                    value={value}
-                    onChange={handleChange}
                     autoComplete="shipping address-line1"
                     name="address"
                     placeholder="Address"
                     className="text-base"
                 />
-            </StandaloneSearchBox>
+            </Autocomplete>
         </div>
     )
 }
